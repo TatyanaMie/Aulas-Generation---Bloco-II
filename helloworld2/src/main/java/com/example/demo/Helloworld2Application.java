aa3191e0ad8e0d964651d8b4af31e7e58172438a
Tatyana Mie <tatyanamie@gmail.com>
2022-01-17 20:27:25 -0300

    primeiro commit de spring

---
+++ helloworld2/src/main/java/com/example/demo/Helloworld2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloworld2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helloworld2Application.class, args);
+	}
+
+}
